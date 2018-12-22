3953781db0269a04ed0a9cbc64d069c342bc583c
Nick Farrow <farrow.nick@gmail.com>
2018-12-22 04:11:54 -0600

    Camera Controls have been updated
    Camera follows anything smoothly now.

--- Balli/Assets/Scripts/CameraSystem.cs
+++ Balli/Assets/Scripts/CameraSystem.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class CameraSystem : MonoBehaviour {
 
-	private GameObject player;
+	public GameObject followTarget;
+	private Vector3 targetPos;
+	public float moveSpeed;
+
 	public float xMin;
 	public float xMax;
 	public float yMin;
@@ -13,18 +16,30 @@ public class CameraSystem : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Screen.orientation = ScreenOrientation.Portrait;
-		player = GameObject.FindGameObjectWithTag ("Player");
+	//	Screen.orientation = ScreenOrientation.Portrait;
+		//player = GameObject.FindGameObjectWithTag ("Player");
 	}
 
 	// Update is called once per frame
 	void Update () {
+		
 
+		targetPos = new Vector3 (followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+		transform.position = Vector3.Lerp(
+			transform.position,											//Position Currently at //FROM
+			targetPos, 													//Position Want to be at //TO
+			moveSpeed * Time.deltaTime  );								// At what speed
+										
 	}
 	// Last Update is called once per frame
 	void LateUpdate () {
-		float x = Mathf.Clamp (player.transform.position.x, xMin, xMax);
-		float y = Mathf.Clamp (player.transform.position.y, yMin, yMax);
+		Clamping();
+	}
+
+
+	void Clamping (){
+		float x = Mathf.Clamp (followTarget.transform.position.x, xMin, xMax);
+		float y = Mathf.Clamp (followTarget.transform.position.y, yMin, yMax);
 		gameObject.transform.position = new Vector3 (x, y, gameObject.transform.position.z);
 	}
 }
