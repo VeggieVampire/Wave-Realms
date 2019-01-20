25df4d65091a4c85a7e88d59f712ffec597857fa
Nick Farrow <farrow.nick@gmail.com>
2019-01-19 22:41:36 -0600

    updates
    I have updated the door frame and worknig on an new animation for when something is touched.

--- Balli/Assets/Scripts/smash.cs
+++ Balli/Assets/Scripts/smash.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class smash : MonoBehaviour {
-
+	float dustRR = 1f;
+	public Transform dust;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +12,20 @@ public class smash : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float dustSpeed = 500f;
+
+		Transform LeftDust = Instantiate (dust, new Vector3 (this.gameObject.transform.position.x, this.gameObject.transform.position.y, 0f), Quaternion.identity);
+		LeftDust.gameObject.AddComponent<Rigidbody2D>();
+		LeftDust.transform.gameObject.GetComponent<Rigidbody2D> ().gravityScale = 0;
+		//LeftDust.gameObject.GetComponent<Rigidbody2D>().AddForce (this.gameObject.transform.position.x + dustRR,this.gameObject.transform.position.y + dustRR  * dustSpeed);
+		//Quaternion target = Quaternion.Euler(0f, dustRR, 0f);
+
+
+		LeftDust.gameObject.GetComponent<Rigidbody2D>().AddForce (Vector2.up * dustSpeed);
+		LeftDust.transform.rotation = Quaternion.Euler(Vector3.left);
+		dustRR ++;
+		if (dustRR >= 360) {
+			dustRR = 1;
+		}
 	}
 }
