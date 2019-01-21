012bfa053b551e134c185bbad57d40766dc63485
Nick Farrow <farrow.nick@gmail.com>
2019-01-21 04:14:28 -0600

    loads of updates

---
+++ Balli/Assets/Scripts/PlayerStartPoint.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStartPoint : MonoBehaviour {
+
+
+	//tells player and cameraSystem to start at
+	private PlayerController thePlayer;
+	private CameraSystem theCamera;
+
+	public Vector2 startDirection;
+
+	// Use this for initialization
+	void Start () {
+		//Finds the Player
+		thePlayer = FindObjectOfType<PlayerController> ();
+
+		//X0,Y0 = down
+		//X0,Y1 = up
+		//X1,Y0 = left
+		//X1,Y1 = right
+		thePlayer.lastMove = startDirection;
+
+		//Moves the Player
+		thePlayer.transform.position = transform.position;
+
+		//Finds the Camera
+		theCamera = FindObjectOfType<CameraSystem> ();
+		//Moves only X and Y. Keeps local Z 
+		theCamera.transform.position = new Vector3 (transform.position.x, transform.position.y, theCamera.transform.position.z);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Balli/Assets/Scripts/SlugoController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlugoController : MonoBehaviour {
+
+	public float moveSpeed;
+	private Rigidbody2D myRigidbody;
+	private bool moving;
+
+	public float timeBetweenMove;
+	private float timeBetweenMoveCounter;
+
+	public float timeToMove;
+	private float timeToMoveCounter;
+
+	private Vector3 MoveDirection;
+	// Use this for initialization
+	void Start () {
+		moving = false;
+		myRigidbody = GetComponent<Rigidbody2D> ();
+
+		//timeBetweenMoveCounter = timeBetweenMove;
+		//timeToMoveCounter = timeToMove;
+		timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
+		timeToMoveCounter = Random.Range(timeToMove * 0.75f,timeToMove * 1.25f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (moving) { //moving
+			timeToMoveCounter -= Time.deltaTime; //one screen refresh
+			myRigidbody.velocity = MoveDirection;
+			if (timeToMoveCounter < 0f) { //if counter has finished for timeToMoveCounter
+				moving = false;
+				//timeBetweenMoveCounter = timeBetweenMove;
+				timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
+			}
+
+		} else { //not moving
+			timeBetweenMoveCounter -= Time.deltaTime; //one screen refresh
+			myRigidbody.velocity = Vector2.zero;
+			if (timeBetweenMoveCounter < 0f) { //if counter has finished for timeBetweenMove
+				moving = true;
+				//timeToMoveCounter = timeToMove; // reset counter for timeToMoveCounter
+				timeToMoveCounter = Random.Range(timeToMove * 0.75f,timeToMove * 1.25f);
+				float RRx = Random.Range(-1f,1f); //Random number between 0-1
+				float RRy = Random.Range(-1f,1f);//Random number between 0-1
+				MoveDirection = new Vector3(RRx * moveSpeed,RRy * moveSpeed,transform.position.z); //Set direction
+				//MoveDirection = new Vector3(RRx * moveSpeed,RRy * moveSpeed,0f);
+
+			}
+		}
+	}
+}
